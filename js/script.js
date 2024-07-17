dc91f562d2b56b0eee9b28dc58a0446ce0f9113f
Artem Knyshevich <knyshevichartem@gmail.com>
2024-07-17 20:53:17 +0300

    fix bug

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ const navLinks = document.querySelector(".nav-links")
 menuHamburger.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-menu')
 })
+
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "avinash6252@gmail.com",
+        Password: "password",
+        To: 'knyshevichartem@gmail.com',
+        From: document.getElementById("exampleInputEmail1").value,
+        Subject: 'Hi',
+        Body: 'Hello',
+    }).then(message => alert(message)
+    );
+
+}
