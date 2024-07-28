56aa71ae16d1fe9a2cbadbe39dfa60ce47082d37
Artem Knyshevich <knyshevichartem@gmail.com>
2024-07-28 15:46:30 +0300

    added

--- js/script.js
+++ js/script.js
@@ -6,16 +6,3 @@ menuHamburger.addEventListener('click', () => {
 })
 
 
-function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "avinash6252@gmail.com",
-        Password: "password",
-        To: 'knyshevichartem@gmail.com',
-        From: document.getElementById("exampleInputEmail1").value,
-        Subject: 'Hi',
-        Body: 'Hello',
-    }).then(message => alert(message)
-    );
-
-}
